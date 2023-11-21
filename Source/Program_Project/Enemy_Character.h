14edb25e03c94d8e1236addf38c6d8dea2fbb274
Haxosura <123653087+Haxosura@users.noreply.github.com>
2023-11-21 09:40:51 +0000

    Patrol Path Setup
    Added Script for Patrol Path Actor
    Added BP_PatrolPath and Points
    Added BP_PatrolPath to Enemy character
    Updated EnemyCharacter Scripts

--- Source/Program_Project/Enemy_Character.h
+++ Source/Program_Project/Enemy_Character.h
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PatrolPath.h"
 #include "GameFramework/Character.h"
 #include "Enemy_Character.generated.h"
 
@@ -15,15 +15,17 @@ public:
 	// Sets default values for this character's properties
 	AEnemy_Character();
 
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+    APatrolPath* PatrolPath;
 };
